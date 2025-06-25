6eef76cfe01b11fc3cfe968cd3367cae03798643
Jhon Gonzalez <jhonxy123@gmail.com>
2025-06-24 21:46:46 -0500

    :zap: configurando para celular fisico

--- app/src/main/java/com/example/subastaparcial/data/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/subastaparcial/data/network/RetrofitInstance.kt
@@ -6,7 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     val api: SubastaApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://0.0.0.0:3000")
+            .baseUrl("http://192.168.20.27:3000/")
+            //.baseUrl("http://10.0.2.2:3000/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(SubastaApiService::class.java)
